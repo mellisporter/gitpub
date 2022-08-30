1e2de70e57281191f68bc35773842d9d44bdfd92
Matt Porter <matthew.ellis.porter@gmail.com>
2022-08-30 11:58:26 -0400

    include get route /

--- server.js
+++ server.js
@@ -3,5 +3,9 @@ const app = express();
 const port = 3000;
 
 app.listen(port, ()=>{
-    console.log("Ready to rumble")
+    console.log("Ready to Rumble")
+})
+
+app.get("/", function(req,res){
+    res.send('Welcome to the Gitpub App!')
 })
