8f9ea5b5701088ee1f9a210f16a73b24c8b01c69
Matt Porter <matthew.ellis.porter@gmail.com>
2022-08-30 17:58:01 -0400

    food index setup

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const port = 3000;
 
 const drinks = require("./models/drinks.js");
 
+// have to require the food data as well
+
+const food = require("./models/food.js");
+
 // we need to specify which variable (in case there were multiple)
 
 
@@ -25,6 +29,15 @@ app.get("/drinks", function(req, res){
         allDrinks: drinks
     })
 })
+
+// set up similar page for food
+app.get("/food", function (req, res){
+    res.render("food_index.ejs", {
+        allFood: food
+    })
+})
+        
+
 // set up new route for drinks/id
 // used as show page
 
@@ -32,6 +45,12 @@ app.get("/drinks/:id", function(req, res){
     res.render("drinks_show.ejs", {
         // need to add parameter here so that the show page can access the name and other data
         drink: drinks[req.params.id]
+    })
+})
+
+app.get("/food/:foodId", function (req, res){
+    res.render("food_show.ejs", {
+      foodItem: food[req.params.id]  
     })
 })
 
