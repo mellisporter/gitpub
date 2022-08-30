8c283f81b9987ae23bd738c16a6de2541e52f069
Matt Porter <matthew.ellis.porter@gmail.com>
2022-08-30 12:20:31 -0400

    drink array loads

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// we need to require the drinks data
+
+const drinks = require("./models/drinks.js");
+
+// we need to specify which variable (in case there were multiple)
+
+
 app.listen(port, ()=>{
     console.log("Ready to Rumble")
 })
 
 app.get("/", function(req,res){
     res.send('Welcome to the Gitpub App!')
+})
+
+app.get("/drinks", function(req, res){
+    res.send(drinks)
 })
